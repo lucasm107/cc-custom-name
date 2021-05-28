951b33e283187a16c29517cb31ee67724e47fa27
Diego Ceraso <diego.ceraso@newtech.com.ar>
2021-05-28 13:12:24 -0300

    delete specific teams send function

--- Source/CompanyCommunicator.Prep.Func/PreparingToSend/Orchestrators/SyncRecipientsOrchestrator.cs
+++ Source/CompanyCommunicator.Prep.Func/PreparingToSend/Orchestrators/SyncRecipientsOrchestrator.cs
@@ -51,6 +51,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend
             // Members of specific teams.
             if (notification.Rosters.Any())
             {
+                /*
                 var tasks = new List<Task>();
                 foreach (var teamId in notification.Rosters)
                 {
@@ -64,6 +65,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend
                 // Fan-Out Fan-In.
                 await Task.WhenAll(tasks);
                 return;
+                */
             }
 
             // Members of M365 groups, DG or SG.
